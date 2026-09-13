77aee5db9fb845e4555dce428bc7740887b7139f
GVHBOX <kapper1659435364@gmail.com>
2026-09-13 19:36:39 +0800

    chore: v1.0.6 默认窗口 1400x900

--- app/shell.py
+++ app/shell.py
@@ -12,8 +12,8 @@ from . import APP_TITLE, APP_TITLE_FULL, __version__, api, log, single, store
 
 logger = log.get_logger(__name__)
 
-WIDTH = 1341
-HEIGHT = 687
+WIDTH = 1400
+HEIGHT = 900
 MIN_WIDTH = 960
 MIN_HEIGHT = 640
 BACKGROUND = "#FFFFFF"

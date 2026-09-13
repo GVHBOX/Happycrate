5ea0aa5d08eb2e792abcf75f3a46de0760017bdd
GVHBOX <kapper1659435364@gmail.com>
2026-09-13 15:40:52 +0800

    chore: v1.0.1，约定每次提交递增版本第三位

--- web/js/api.js
+++ web/js/api.js
@@ -247,7 +247,7 @@
 
     appInfo: function(){
       if (live()) return window.pywebview.api.app_info();
-      return Promise.resolve({version:"1.0.0", dataDir:"(mock 模式)", mode:"mock"});
+      return Promise.resolve({version:"1.0.1", dataDir:"(mock 模式)", mode:"mock"});
     },
 
     onSearch: function(hooks){ sHooks = hooks || {}; },
